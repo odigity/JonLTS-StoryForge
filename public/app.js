cda6f53cc2ac09260d8fe31eb548a3d957d631bc
Ofer Nave <odigity@gmail.com>
2016-11-06 06:21:22 -0800

    prettied up, layout developed more, some content mocking

--- public/app.js
+++ public/app.js
@@ -1,26 +1,69 @@
 
-//    ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById('root') );
-
     var BrowserRouter   = ReactRouter.BrowserRouter,
         Link            = ReactRouter.Link,
         Match           = ReactRouter.Match,
         Miss            = ReactRouter.Miss;
 
     const App = () => (
-        <BrowserRouter>
-            <div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
-
-                <Match exactly pattern="/" component={Home} />
-                <Match pattern="/about" component={About} />
-            </div>
-        </BrowserRouter>
+        <div id="app">
+            <Header />
+            <Match exactly pattern="/" component={Home} />
+            <Match pattern="/about"    component={About} />
+            <Match pattern="/browse"   component={Browse} />
+            <Footer />
+        </div>
     )
-    const Home   = () => ( <div><h2>Home</h2></div> )
+
+    const Header = () => (
+        <nav id="header">
+            <ul>
+                <li><Link to="/browse">Browse Stories</Link></li>
+{/*
+                <li><a href="username1">My Stories</a></li>
+                <li><a href="new-story">New Story</a></li>
+*/}
+                <li><Link to="/about">About</Link></li>
+            </ul>
+            <h1><Link to="/">StoryForge</Link></h1>
+        </nav>
+    )
+
+    const Footer = () => (
+        <div id="footer" className="panel">&copy; 2016 Jon Pawelko</div>
+    )
+
+    const Home = () => (
+        <div>
+            <Welcome />
+            <NewStories />
+        </div>
+    )
+
+    const Welcome = () => (
+        <div className="panel">Welcome to StoryForge! Insert explanation here...</div>
+    )
+
+    const NewStories = () => (
+        <div className="panel">
+            <h2>New Stories</h2>
+            <ul className="reset">
+                <li><StoryItem /></li>
+            </ul>
+        </div>
+    )
+
+    const Browse = () => ( <div><h2>Browse</h2></div> )
     const About  = () => ( <div><h2>About</h2></div> )
 
-    ReactDOM.render( <App />, document.getElementById('root') );
+    const StoryItem = () => (
+        <div className="story-item">
+            <img src="/story.png" />
+            <h3><a href="#">name</a> by <a href="#">author</a></h3>
+            <h4>Started on created_at &mdash; node_count nodes</h4>
+            <p>description</p>
+        </div>
+    )
+
+    ReactDOM.render( <BrowserRouter><App /></BrowserRouter>, document.getElementById('root') );
+
 
