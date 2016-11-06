6431388635c96ba86425d985fa64ad2fd6899f2f
Ofer Nave <odigity@gmail.com>
2016-11-06 05:43:27 -0800

    added React Router with minimal working example

--- public/app.js
+++ public/app.js
@@ -1,3 +1,26 @@
 
-ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById('root') );
+//    ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById('root') );
+
+    var BrowserRouter   = ReactRouter.BrowserRouter,
+        Link            = ReactRouter.Link,
+        Match           = ReactRouter.Match,
+        Miss            = ReactRouter.Miss;
+
+    const App = () => (
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+
+                <Match exactly pattern="/" component={Home} />
+                <Match pattern="/about" component={About} />
+            </div>
+        </BrowserRouter>
+    )
+    const Home   = () => ( <div><h2>Home</h2></div> )
+    const About  = () => ( <div><h2>About</h2></div> )
+
+    ReactDOM.render( <App />, document.getElementById('root') );
 
